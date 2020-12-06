687ed850250118a491937aa53f5c76ffb5350559
LUISCDF <luiscdf@ua.pt>
2020-12-06 13:25:41 +0000

    almost finishing report

--- stats_versions/dp_smith_waterman.py
+++ stats_versions/dp_smith_waterman.py
@@ -1,27 +1,35 @@
 import numpy as np
 import sys
+import time
 from constants import *
 from backtrace import backtrace
 
+basic_operations = 0
 
 def smith_waterman(word01, word02):
     print("lines:", len(word01) + 1, "| cols:", len(word02) + 1)
     matrix = np.zeros((len(word01) + 1, len(word02) + 1))
     directions_matrix = np.zeros((len(word01) + 1, len(word02) + 1))
 
+    initial_time = time.time()
     calc_matrix_scores(matrix, directions_matrix, word01, word02)
-    return backtrace(matrix, directions_matrix, word01, word02)
+    mid_time = time.time()
+    val = backtrace(matrix, directions_matrix, word01, word02)
+    final_time = time.time()
+    print("Scoring:", mid_time - initial_time, "backtrace:", final_time - mid_time, "total:", final_time - initial_time)
+    return val
 
 
 # DP
 def calc_matrix_scores(matrix, directions_matrix, word01, word02):
     for col in range(1, len(word02) + 1):
         for lin in range(1, len(word01) + 1):
+            global basic_operations
+            basic_operations += 3
             equality = MATCH_BONUS if (word02[col - 1] == word01[lin - 1]) else MISMATCH_PENALTY
             v_up = matrix[lin - 1, col] + GAP_PENALTY
             v_left = matrix[lin, col - 1] + GAP_PENALTY
             v_diagonal = matrix[lin - 1, col - 1] + equality
-
             matrix[lin, col] = max([0, v_up, v_left, v_diagonal])
             directions_matrix[lin, col] = np.argmax([0, v_up, v_left, v_diagonal])
 
@@ -30,3 +38,4 @@ if __name__ == "__main__":
     # Args
     arg_word01, arg_word02 = sys.argv[1].upper(), sys.argv[2].upper()
     print(smith_waterman(arg_word01, arg_word02))
+    print(basic_operations)
--- stats_versions/memo_smith_waterman.py
+++ stats_versions/memo_smith_waterman.py
@@ -2,7 +2,10 @@ import numpy as np
 import sys
 from constants import *
 from backtrace import backtrace
+import time
 
+basic_operations = 0
+function_call = 0
 
 def smith_waterman(word01, word02):
     print("lines:", len(word01) + 1, "| cols:", len(word02) + 1)
@@ -10,14 +13,23 @@ def smith_waterman(word01, word02):
     matrix[0, :] = 0
     matrix[:, 0] = 0
     directions_matrix = np.zeros((len(word01) + 1, len(word02) + 1))
-
+    initial_time = time.time()
     calc_matrix_scores(matrix, directions_matrix, word01, word02, len(word01), len(word02))
-    return backtrace(matrix, directions_matrix, word01, word02)
+    mid_time = time.time()
+    val = backtrace(matrix, directions_matrix, word01, word02)
+    final_time = time.time()
+    print("Scoring:", mid_time - initial_time, "backtrace:", final_time - mid_time, "total:", final_time - initial_time)
+    return val
 
 
 def calc_matrix_scores(matrix, directions_matrix, word01, word02, x, y):
+    global function_call
+    function_call += 1
     if matrix[x, y] != -1:
         return matrix[x, y]
+
+    global basic_operations
+    basic_operations += 3
 
     eq = (MATCH_BONUS if (word02[y - 1] == word01[x - 1]) else MISMATCH_PENALTY)
     v_diagonal = calc_matrix_scores(matrix, directions_matrix, word01, word02, x - 1, y - 1) + eq
@@ -33,3 +45,4 @@ if __name__ == "__main__":
     # Args
     arg_word01, arg_word02 = sys.argv[1].upper(), sys.argv[2].upper()
     print(smith_waterman(arg_word01, arg_word02))
+    print(basic_operations, function_call)
